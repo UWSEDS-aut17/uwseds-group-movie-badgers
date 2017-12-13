15e7108f552602c64d2024181e1e3e09d493329c
unknown <anna.huang.us@gmail.com>
2017-12-12 23:00:10 -0800

    updated for pep8

--- movie_badgers/tests/test_clean_data.py
+++ movie_badgers/tests/test_clean_data.py
@@ -1,20 +1,22 @@
-import unittest
 import sys
-sys.path.append('../')
-import clean_data as cd
+import unittest
 import os
+sys.path.append('../')
+import clean_data as cd   # noqa
+
 
 class FunctionRunTest(unittest.TestCase):
     """Test if functions run succesfully with correct input"""
-    
+
     def test_get_act_pop_avg(self):
         cd.get_act_pop_avg()
-    
+
     def test_clean_director_actor(self):
         cd.clean_director_actor()
 
     def test_clean_regression_data(self):
         cd.clean_regression_data()
+
 
 if __name__ == '__main__':
     unittest.main()
